ad6db24dbd90efd9c8e4a2be3678e4123da6f557
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-06-11 08:01:21 -0700

    verify instructor api

--- index.js
+++ index.js
@@ -53,12 +53,22 @@ async function run() {
       res.send({ token });
     });
 
-    // verify andmin instructor and student
+    // verify admin instructor and student
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       if (user?.role !== "admin") {
+        return res.status(403).send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+
+    const verifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "instructor") {
         return res.status(403).send({ error: true, message: "forbidden message" });
       }
       next();
